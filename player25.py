1225543847867c87300fd775d3eb5cbee4a7ac43
TechnoHwizrdry <alex.mardigian@gmail.com>
2023-06-07 00:34:29 -0400

    Refactoring
    Replaced hardcored string with a command line argument.  This was done
    to improve flexibility off the app.

--- player25.py
+++ player25.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# RaspiPlayer 640x430 - LARGE -screen
-# This is to be used with a 3.5" HDMI touchscreen or equivalent
-# Tested with the Raspberry pi 2 and raspbian stretch
+# RaspiPlayer 800x480 - LARGE -screen
+# This is to be used with a 4.3" TFT touchscreen or equivalent
+# Tested with the Raspberry pi 3 and raspbian bullseye
 # The program must be run within the Lxterminal.
 # 
-# 
-# Rev2.53 by Granpino, Dec2020
+# Modified by Alexan Mardigian, June 2023
+# Based on Rev2.53 by Granpino, Dec2020
  
 import sys, pygame
+from argparse import ArgumentParser, Namespace
 from pygame.locals import *
 import time
 import datetime
@@ -30,7 +31,7 @@ silver = 192, 192, 192
 gray = 40, 40, 40
 
 #other
-os.system("mount /dev/sda1 /mnt/usbdrive") #setup for USB drive
+#os.system("mount /dev/sda1 /mnt/usbdrive") #setup for USB drive
 subprocess.call("mpc stop", shell=True)
 subprocess.call("mpc random off", shell=True)
 subprocess.call("mpc clear", shell=True)
@@ -69,6 +70,22 @@ size = width, height = 640, 430 #change as required
 ### remove FULLSCREEN mode for troubleshooting purposes
 #screen = pygame.display.set_mode(size)
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+
+def get_args() -> Namespace:
+	parser = ArgumentParser()
+	parser.add_argument(
+		'-m',
+		dest='music_path',
+		help='Path to directory with music.',
+		required=True
+	)
+
+	# If no arguments were provided, then print help and exit.
+	if len(sys.argv) == 1:
+		parser.print_help()
+		sys.exit(1)
+
+	return parser.parse_args()
 
 #define function that checks for mouse location
 def on_click():
@@ -114,7 +131,7 @@ def on_click():
 	if round(410*q,0) <= click_pos[0] <= round(458*q,0) and round(62*q,0) <= click_pos[1] <=round(100*q,0):
 		button(10)
 
-     # seek - 11, button was pressed
+	 # seek - 11, button was pressed
 	if round(52*q,0) <= click_pos[0] <= round(458*q,0) and round(250*q,0) <= click_pos[1] <=round(275*q,0):
 		button(11)
 
@@ -148,8 +165,8 @@ def button(number):
 						pygame.display.flip()
 						subprocess.call("mpc stop", shell=True)
 						os.system("sudo shutdown -h now")
-				if event.type == KEYDOWN:
-					if event.key == K_ESCAPE: # ESC to exit
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_ESCAPE: # ESC to exit
 						subprocess.call("mpc stop", shell=True)
 						sys.exit()
 			pygame.display.flip()
@@ -165,8 +182,11 @@ def button(number):
 		album_img = ("/tmp/kunst.png")
 	#	refresh_screen()
 
+	music_path = opts.music_path
+	ls_music_path = f"ls {music_path}"
+
 	if number == 2: # FOLDER PLAY.
-		usbdrv2 = subprocess.check_output("ls /mnt/usbdrive", shell=True)
+		usbdrv2 = subprocess.check_output(ls_music_path, shell=True)
 		if len(usbdrv2)==0: # is USB connected?
 			CurrPlaylist = "No USB!"
 			return
@@ -193,7 +213,7 @@ def button(number):
 			#	refresh_screen()
 
 	if number == 3: # MP3 PLAY
-		usbdrv = subprocess.check_output("ls /mnt/usbdrive", shell=True)
+		usbdrv = subprocess.check_output(ls_music_path, shell=True)
 		if len(usbdrv)==0:  # is USB connected?
 			CurrPlaylist = "No USB!"
 			return
@@ -280,82 +300,82 @@ def button(number):
 			return
 
 def connected():
-    #Detect an internet connection
-    return # this is not reliable
-    global connection
-    connection = None
-    try:
-        socket.create_connection(("1.1.1.1", 53)) # check every 180 seconds
+	#Detect an internet connection
+	return # this is not reliable
+	global connection
+	connection = None
+	try:
+		socket.create_connection(("1.1.1.1", 53)) # check every 180 seconds
 #        print("Internet connection detected.")
-        connection = True
-    except OSError:
+		connection = True
+	except OSError:
 #        print("Internet connection not detected.")
-        connection = False
-    finally:
-        return connection
+		connection = False
+	finally:
+		return connection
 
 def show_current():
  ##### display the station name and split it into 2 parts : 
-    try:
-        tag = subprocess.check_output("mpc current", shell=True).decode(sys.stdout.encoding).split(" - ")
-    except subprocess.CalledProcessError:
-      #  subprocess.call("mpc next", shell=True)
-        return
-    if len(tag)==1:
-        artist = tag[0]
-        title = " RaspiPlayer: "
-    else:
-        artist = tag[0]
-        title = tag[1]
+	try:
+		tag = subprocess.check_output("mpc current", shell=True).decode(sys.stdout.encoding).split(" - ")
+	except subprocess.CalledProcessError:
+	  #  subprocess.call("mpc next", shell=True)
+		return
+	if len(tag)==1:
+		artist = tag[0]
+		title = " RaspiPlayer: "
+	else:
+		artist = tag[0]
+		title = tag[1]
 
-    artist = artist[:38]
-    title = title[:38]
-    title = title[:-1]
-    #trap no station data
-    if artist =="":
-        title = "Press PLAY"
-        Playlist_name = (CurrPlaylist)
-        play = False
-    else:
-        Playlist_name = (CurrPlaylist)
+	artist = artist[:38]
+	title = title[:38]
+	title = title[:-1]
+	#trap no station data
+	if artist =="":
+		title = "Press PLAY"
+		Playlist_name = (CurrPlaylist)
+		play = False
+	else:
+		Playlist_name = (CurrPlaylist)
 
-    artist_name=sfont.render(artist, 1, (white))
-    title_lbl=sfont.render(title, 1, (white))
-    playlist_label=sfont.render(Playlist_name, 1, (cyan))
-    screen.blit(playlist_label,(int(190*q),int(120*q))) #playlist
-    screen.blit(artist_name,(int(66*q),int(225*q)))
-    screen.blit(title_lbl,(int(66*q),int(195*q)))
+	artist_name=sfont.render(artist, 1, (white))
+	title_lbl=sfont.render(title, 1, (white))
+	playlist_label=sfont.render(Playlist_name, 1, (cyan))
+	screen.blit(playlist_label,(int(190*q),int(120*q))) #playlist
+	screen.blit(artist_name,(int(66*q),int(225*q)))
+	screen.blit(title_lbl,(int(66*q),int(195*q)))
 
 def Rem_time():
-    pygame.draw.line(screen, black, (52*q, 260*q), (459*q, 260*q), 10)
+	pygame.draw.line(screen, black, (52*q, 260*q), (459*q, 260*q), 10)
 
-     ## display remaining time in %
-    try: 
-	    RemTime = subprocess.check_output("mpc -f  %time%", shell=True).decode(sys.stdout.encoding).split("\n")
-    except subprocess.CalledProcessError:
-        return
-    if len(RemTime)==0: # percent time played
-        remT1 = RemTime[0]
-        remT1 = remT1[:-1]
+	 ## display remaining time in %
+	try:
+		RemTime = subprocess.check_output("mpc -f  %time%", shell=True).decode(sys.stdout.encoding).split("\n")
+	except subprocess.CalledProcessError:
+		return
+	
+	if len(RemTime)==0: # percent time played
+		remT1 = RemTime[0]
+		remT1 = remT1[:-1]
+	else:
+		remT1 = RemTime[0]
+		remT2 = RemTime[1]
 
-    else:
-        remT1 = RemTime[0]
-        remT2 = RemTime[1]
-
-    if mp3 == True and play == True:  #Draw bargraph
-        try:
-            str1 = remT2[-5:-2]
-            str1 = str1.replace("(", '')
-            str1 = int(str1)
-            # Multiplier =  (407/100)*q or (bargraph length / 100%)
-            # multiply by 5.4 for 640x430 and 4.07 for 480x320
-            pygame.draw.line(screen, green, (52*q, 260*q), (52*q + (str1*4.07*q), 260*q), 8)
-        except ValueError:
-            subprocess.call("mpc stop", shell=True)
-            print('value error')
-    remT2 = remT2[:-5]
-    rem_time=sfont.render(remT2, 1, (cyan))
-    screen.blit(rem_time,(int(190*q),int(153*q)))
+	if mp3 == True and play == True:  #Draw bargraph
+		try:
+			str1 = remT2[-5:-2]
+			str1 = str1.replace("(", '')
+			str1 = int(str1)
+			# Multiplier =  (407/100)*q or (bargraph length / 100%)
+			# multiply by 5.4 for 640x430 and 4.07 for 480x320
+			pygame.draw.line(screen, green, (52*q, 260*q), (52*q + (str1*4.07*q), 260*q), 8)
+		except ValueError:
+			subprocess.call("mpc stop", shell=True)
+			print('value error')
+	remT2 = remT2[:-5]
+	rem_time=sfont.render(remT2, 1, (cyan))
+	screen.blit(rem_time,(int(190*q),int(153*q)))
 
 def refresh_screen():
 	global connect_img
@@ -395,7 +415,7 @@ def refresh_screen():
 
 	##### display the station name and split it into 2 parts : 
 	show_current()
-        #remaining time
+		#remaining time
 	Rem_time()
 	# add volume number
 #	volume = subprocess.check_output("mpc volume", shell=True )
@@ -425,7 +445,10 @@ def refresh_screen():
 def main():
 	global click_pos
 	global seek
+	global opts
+	opts = get_args()
 	timer = pygame.time.get_ticks()
+	
 	while 1:
 		seconds=(pygame.time.get_ticks() - timer)/1000
 		if seconds > 180: # check every 3 min 
@@ -433,15 +456,15 @@ def main():
 			connected() # check for internet connection
 
 		for event in pygame.event.get():
-			if event.type == MOUSEBUTTONDOWN:
+			if event.type == pygame.MOUSEBUTTONDOWN:
 				click_pos = event.pos
 			#	print (event)
 				print (event.pos)
 				seek = event.pos[0] # click position x
 				on_click()
 
-			if event.type == KEYDOWN:
-				if event.key == K_ESCAPE: # ESC key will kill it
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE: # ESC key will kill it
 					sys.exit()
 		clock.tick(5) #refresh screen fps 
 		refresh_screen()
--- radio.sh
+++ radio.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #radio.sh
 
-sudo python player25.py
+sudo python player25.py -m /home/alexan/Music/
