93554da103081998fa17f32abbbfb5ee2d7afd96
TechnoHwizrdry <alex.mardigian@gmail.com>
2023-06-08 09:58:25 -0400

    Added cmd line parameter to radio.sh
    This was done to reduce hardcoded values in the code, and improve
    flexibility.

--- radio.sh
+++ radio.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 #radio.sh
-
-sudo python player25.py -m /home/alexan/Music/
+if [ -z "$1" ]
+then
+    echo "usage: $0 USERNAME_HERE"
+else
+    sudo python player25.py -m /home/$1/Music/
+fi
