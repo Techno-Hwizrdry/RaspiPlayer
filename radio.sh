9fb04c090788a73365c7c45dd1ec03d866be7960
TechnoHwizrdry <alex.mardigian@gmail.com>
2023-06-08 12:24:50 -0400

    Refactored if else statement to just one if.

--- radio.sh
+++ radio.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 #radio.sh
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo "usage: $0 USERNAME_HERE"
-else
-    sudo python player25.py -m /home/$1/Music/
+    exit 1
 fi
+sudo python player25.py -m /home/$1/Music/
